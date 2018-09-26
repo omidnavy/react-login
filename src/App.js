bc7d738d443f8375d7819932ddba5b4fe2fb76a5
Omid Navy <omidnavy@yahoo.com>
2018-09-26 18:01:52 +0330

    Logo Changed Lorem Ipsum added button created inputs css changed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            renderLeft: true
+        }
+    }
+
+    componentWillMount() {
+        this.fixSize();
+        document.querySelector('body').onresize = () => this.fixSize()
+    }
+
+    fixSize() {
+        if (document.querySelector('body').offsetWidth < 1089 && this.state.renderLeft === true) this.setState({renderLeft: false})
+        else if (document.querySelector('body').offsetWidth >= 1089 && this.state.renderLeft === false) this.setState({renderLeft: true})
+    }
+
+    login() {
+        this.props.onLogin()
     }
 
     render() {
         return (
             <div className="l6xt">
-                <div className="l6xt-left">
-                    <div className="l6xt-header">
-                        <img src={this.props.logo} />
+                {this.state.renderLeft && (
+                    <div className="l6xt-left">
+                        <div className="l6xt-header">
+                            <img src={this.props.logo}/>
+                        </div>
+                        <div className="l6xt-content">
+                            <h1>{this.props.h1}</h1>
+                            <h4>{this.props.h4}</h4>
+                            <p>{this.props.p}</p>
+                        </div>
+                        {this.props.footer && (
+                            <div className="l6xt-footer">
+                                {
+                                    this.props.footer.map((link, index) => {
+                                        return (<a key={index} href={link.href}>{link.value}</a>)
+                                    })
+                                }
+                            </div>
+                        )}
+
                     </div>
-                    <div className="l6xt-content">
-                        <h1>{this.props.h1}</h1>
-                        <h4>{this.props.h4}</h4>
-                        <p>{this.props.p}</p>
-                    </div>
-                    <div className="l6xt-footer">
-                        <a href="#">Help Center</a>
-                        <a href="#">Website</a>
-                    </div>
-                </div>
+                )}
                 <div className="l6xt-right">
 
                     <div className="l6xt-header">
@@ -33,24 +57,26 @@ class App extends Component {
                     <div className="l6xt-content">
                         <form>
                             <div style={{margin: 40}}>
-                                <h1 >Login</h1>
+                                <h1>Login</h1>
                             </div>
                             <div style={{margin: 40}}>
-                                <input placeholder="Username"/>
+                                <input onChange={(e) => this.props.onUsernameChange(e.target.value)} type='text'
+                                       placeholder="Username"/>
                             </div>
                             <div style={{margin: 40}}>
-                                <input placeholder="Password"/>
+                                <input onChange={(e) => this.props.onPasswordChange(e.target.value)} type='password'
+                                       placeholder="Password"/>
                             </div>
                             <div style={{margin: 40}}>
-                                <button placeholder="Password"/>
+                                <input onClick={() => this.login()} type="button" value="Login"/>
                             </div>
                         </form>
                     </div>
 
                 </div>
             </div>
-    );
+        );
     }
-    }
+}
 
-    export default App;
+export default App;
